88ae8bfca0725eb4433d196cfa0cee4120e41f33
Gregory Potemkin <gregory@gomobile.ru>
2022-08-02 22:02:50 +0300

    fix

--- flexq/jobqueues/postgres_jobqueue/postgres_jobqueue.py
+++ flexq/jobqueues/postgres_jobqueue/postgres_jobqueue.py
@@ -2,6 +2,7 @@ import logging
 import select
 from typing import Callable, List, Union
 import psycopg2
+import psycopg2.extensions
 from psycopg2.extensions import Notify
 from flexq.jobqueues.jobqueue_base import JobQueueBase, NotificationTypeEnum
 
@@ -16,35 +17,36 @@ class PostgresJobQueue(JobQueueBase):
     def subscribe_to_queues(self, queues_names: List[str], todo_callback: Union[Callable, None]=None):
         self.todo_callback = todo_callback
 
-        with psycopg2.connect(self.dsn) as conn:
-            conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        conn = psycopg2.connect(self.dsn)
+        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
-            with conn.cursor() as curs:
-                for queue_name in queues_names:
-                    for notification_type in NotificationTypeEnum:
-                        channel_name = f'{queue_name}{parts_join_char}{notification_type}'.lower()
-                        curs.execute(f"LISTEN {channel_name};")
-                        logging.debug(f'Listening for channel {channel_name}')
+        curs = conn.cursor()
+        for queue_name in queues_names:
+            for notification_type in NotificationTypeEnum:
+                channel_name = f'{queue_name}{parts_join_char}{notification_type}'.lower()
+                curs.execute(f"LISTEN {channel_name};")
+                logging.debug(f'Listening for channel {channel_name}')
+                break
 
-                while True:
-                    # read_c, _, exc_c = select.select([conn],[],[])
-                    # logging.debug('select on conn triggered')
-                    # if len(read_c) > 0:
-                    #     conn.poll()
-                    #     while conn.notifies:
-                    #         notify = conn.notifies.pop(0)
-                    #         logging.debug(f"Got NOTIFY: {notify.pid}, {notify.channel}, {notify.payload}")
-                    #         self._handle_notification(notify)
-                    if select.select([conn],[],[],5) == ([],[],[]):
-                        # conn.poll()
-                        logging.debug(f'select read loop, notifies: {conn.notifies}')
-                        # pass
-                    else:
-                        conn.poll()
-                        while conn.notifies:
-                            notify = conn.notifies.pop(0)
-                            logging.debug(f"Got NOTIFY: {notify.pid}, {notify.channel}, {notify.payload}")
-                            self._handle_notification(notify)
+        while True:
+            # read_c, _, exc_c = select.select([conn],[],[])
+            # logging.debug('select on conn triggered')
+            # if len(read_c) > 0:
+            #     conn.poll()
+            #     while conn.notifies:
+            #         notify = conn.notifies.pop(0)
+            #         logging.debug(f"Got NOTIFY: {notify.pid}, {notify.channel}, {notify.payload}")
+            #         self._handle_notification(notify)
+            if select.select([conn],[],[],5) == ([],[],[]):
+                # conn.poll()
+                logging.debug(f'select read loop, notifies: {conn.notifies}')
+                # pass
+            else:
+                conn.poll()
+                while conn.notifies:
+                    notify = conn.notifies.pop(0)
+                    logging.debug(f"Got NOTIFY: {notify.pid}, {notify.channel}, {notify.payload}")
+                    self._handle_notification(notify)
 
     def send_notify_to_queue(self, queue_name: str, notifycation_type: NotificationTypeEnum, payload: str):
         with psycopg2.connect(self.dsn) as conn:
