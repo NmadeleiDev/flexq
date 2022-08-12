e734941ce3b4b2a134af2d95fde5cad6bc6153db
Gregory Potemkin <gregory@gomobile.ru>
2022-08-12 14:35:53 +0300

    update

--- flexq/workers/threading_worker/threading_worker.py
+++ flexq/workers/threading_worker/threading_worker.py
@@ -25,14 +25,16 @@ class ThreadingWorker(WorkerBase):
     def _todo_callback(self, job_name: str, job_id: str):
         logging.debug(f'got job_name={job_name}, job_id={job_id} in _todo_callback')
         if job_id in self.running_jobs:
-            raise RunningJobDuplicate(f'job {job_name} with id={job_id} passed to _todo_callback, but it is already in self.running_jobs')
+            logging.warn(f'job {job_name} with id={job_id} passed to _todo_callback, but it is already in self.running_jobs')
+            return
 
         if job_name not in self.executors.keys():
             if job_name not in (Pipeline.queue_name, Group.queue_name, JobComposite.queue_name):
-                raise UnknownJobExecutor(f'Job executor "{job_name}" is not known here')
+                logging.error(f'Job executor "{job_name}" is not known here')
+                return
         else:
             if self.max_parallel_executors is not None and len(self.running_jobs) >= self.max_parallel_executors:
-                logging.debug(f'skipping job job_name={job_name}, job_id={job_id} due to max amount of parallel executors running')
+                logging.info(f'skipping job job_name={job_name}, job_id={job_id} due to max amount of parallel executors running')
                 return
 
         job_thread = Thread(target=self._try_start_job, args=(job_name, job_id))
